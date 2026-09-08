acc5ad7e15e1df0c7ef43715e80ea0a2397bd070
Aleon Chagas <achagas1@uolinc.com>
2026-09-08 10:41:29 -0300

    feat: habilitar por padrao o agendamento automatico de ligar/desligar instâncias (Resource Scheduler)

--- iac/main-stack/variables.tf
+++ iac/main-stack/variables.tf
@@ -1,134 +1,131 @@
 # OCI Compartment and Authentication
 variable "tenancy_ocid" {
   type        = string
-  description = "OCID of the OCI Tenancy"
+  description = "OCID da Tenancy na OCI"
 }
 
 variable "user_ocid" {
   type        = string
-  description = "OCID of the OCI User"
+  description = "OCID do Usuário na OCI"
 }
 
 variable "fingerprint" {
   type        = string
-  description = "Fingerprint of the OCI RSA API Key"
+  description = "Fingerprint da Chave de API RSA da OCI"
 }
 
 variable "private_key_path" {
   type        = string
-  description = "Path to the OCI API private key"
+  description = "Caminho para a chave privada da API da OCI"
 }
 
 variable "compartment_ocid" {
   type        = string
-  description = "OCID of the OCI Compartment"
+  description = "OCID do Compartimento na OCI"
 }
 
 variable "region" {
   type        = string
-  description = "OCI Region"
+  description = "Região da OCI"
   default     = "us-ashburn-1"
 }
 
 variable "ssh_public_key_path" {
   type        = string
-  description = "Path to the OpenSSH public key for Linux instance access"
+  description = "Caminho para a chave pública OpenSSH de acesso às instâncias Linux"
   default     = "~/.ssh/id_rsa.pub"
 }
 
 # Operating System Selection (Default: Oracle Linux 9 Native OCI)
 variable "os_distribution" {
   type        = string
-  description = "OS Distribution: 'Oracle Linux' (9)"
+  description = "Distribuição do SO: 'Oracle Linux' (9)"
   default     = "Oracle Linux"
 }
 
 variable "os_version" {
   type        = string
-  description = "OS Version"
+  description = "Versão do SO"
   default     = "9"
 }
 
 # Bastion Host (AMD Micro Always Free)
 variable "bastion_shape" {
   type        = string
-  description = "Shape for the Bastion Host (AMD x86_64 Always Free)"
+  description = "Shape para o Bastion Host (AMD x86_64 Always Free)"
   default     = "VM.Standard.E2.1.Micro"
 }
 
 # Kubernetes Cluster Hardware Configuration (ARM Ampere A1 Always Free)
 variable "instance_shape" {
   type        = string
-  description = "Shape for K8s Cluster Nodes (ARM Ampere A1 Flex Always Free)"
+  description = "Shape para os Nós do Cluster K8s (ARM Ampere A1 Flex Always Free)"
   default     = "VM.Standard.A1.Flex"
 }
 
 variable "master_ocpus" {
   type        = number
-  description = "OCPUs for the Master Node"
+  description = "OCPUs para o Nó Master (Control Plane)"
   default     = 2
 }
 
 variable "master_memory_in_gbs" {
   type        = number
-  description = "RAM in GB for the Master Node"
+  description = "Memória RAM em GB para o Nó Master"
   default     = 8
 }
 
 variable "worker_count" {
   type        = number
-  description = "Number of Worker Nodes"
+  description = "Quantidade de Nós Workers"
   default     = 2
 }
 
 variable "worker_ocpus" {
   type        = number
-  description = "OCPUs for each Worker Node"
+  description = "OCPUs para cada Nó Worker"
   default     = 1
 }
 
 variable "worker_memory_in_gbs" {
   type        = number
-  description = "RAM in GB for each Worker Node"
+  description = "Memória RAM em GB para cada Nó Worker"
   default     = 8
 }
 
 variable "boot_volume_size_in_gbs" {
   type        = string
-  description = "Boot volume size in GB"
+  description = "Tamanho do volume de boot em GB"
   default     = "50"
 }
 
 # OCI Resource Scheduler Configuration
 variable "enable_resource_scheduler" {
   type        = bool
-  description = "Enable automated Start/Stop schedules via OCI Resource Scheduler. Set to true after granting 'manage resource-schedule-family' policy to the API/CI-CD user group."
-  default     = false
+  description = "Habilita os agendamentos automáticos de Inicialização / Parada via OCI Resource Scheduler."
+  default     = true
 }
 
 variable "create_scheduler_policy" {
   type        = bool
-  description = "Create IAM Policy via Terraform (Set to false if policy was created manually or if API user lacks IAM Policy management privileges)"
+  description = "Cria a política IAM via Terraform (defina como false se a política foi criada manualmente ou se o usuário da API não possui privilégios de criação de políticas IAM)."
   default     = false
 }
 
 variable "scheduler_time_zone" {
   type        = string
-  description = "Timezone for OCI Resource Scheduler (Default: UTC)"
-  default     = "UTC"
+  description = "Fuso horário para o OCI Resource Scheduler (Padrão: America/Sao_Paulo)"
+  default     = "America/Sao_Paulo"
 }
 
 variable "scheduler_start_cron" {
   type        = string
-  description = "Cron expression for starting compute instances in UTC (Default: 11:30 UTC = 08:30 AM BRT)"
-  default     = "30 11 * * *"
+  description = "Expressão CRON para ligar as instâncias diariamente (Padrão: 08:30 BRT)"
+  default     = "30 8 * * *"
 }
 
 variable "scheduler_stop_cron" {
   type        = string
-  description = "Cron expression for stopping compute instances in UTC (Default: 21:30 UTC = 18:30 PM BRT)"
-  default     = "30 21 * * *"
+  description = "Expressão CRON para desligar as instâncias diariamente (Padrão: 18:30 BRT)"
+  default     = "30 18 * * *"
 }
-
-
-
